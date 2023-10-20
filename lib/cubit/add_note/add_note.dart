f631064eaf410f356ce4fc687854f540b9e2942c
Your Name <you@example.com>
2023-10-20 19:41:37 +0300

    register adapter

--- lib/cubit/add_note/add_note.dart
+++ lib/cubit/add_note/add_note.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
+import 'package:note_app100/models/note_model.dart';
 part 'add_note_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+
+
+  addNote(NoteModel note){
+    
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app100/constans.dart';
+import 'package:note_app100/models/note_model.dart';
 import 'package:note_app100/pages/home_page.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 class NotesApp extends StatelessWidget {
