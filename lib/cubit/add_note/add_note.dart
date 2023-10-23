2b9851fe6df81709138edf6114743122616f9f14
Your Name <you@example.com>
2023-10-23 17:41:58 +0300

    add note method

--- lib/cubit/add_note/add_note.dart
+++ lib/cubit/add_note/add_note.dart
@@ -1,14 +1,27 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app100/constans.dart';
 import 'package:note_app100/models/note_model.dart';
 part 'add_note_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
-
-
-  addNote(NoteModel note){
-    
+  addNote(NoteModel model)async{
+    emit(NotesLoading());
+  try {
+  var noteBox = Hive.box<NoteModel>(kNoteBox);
+  emit(NotesSuccess());
+  await noteBox.add(model);
+ }  catch (e) {
+  NotesFailure(e.toString());
+ }
   }
+
+  // add<T>(T name){
+  //   return name ;
+  // }
 }
+
+
--- lib/cubit/add_note/add_note_state.dart
+++ lib/cubit/add_note/add_note_state.dart
@@ -7,8 +7,7 @@ class NotesInitial extends NotesState {}
 class NotesLoading extends NotesState {}
 class NotesSuccess extends NotesState {}
 class NotesFailure extends NotesState {
-  final String errerMessage;
 
+  final String errerMessage;
   NotesFailure(this.errerMessage);
 }
-
